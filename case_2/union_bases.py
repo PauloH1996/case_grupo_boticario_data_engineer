9279ee5770668450b2d0b2a214ff6a474694fccf
PauloH1996 <56973603+PauloH1996@users.noreply.github.com>
2023-03-30 11:16:09 -0300

    código de union_bases

---
+++ case_2/union_bases.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+local_files = 'base_dados/'
+
+df = []
+
+for file in os.listdir(local_files):
+    if file.endswith('.xlsx'):
+        print(f'Carregando arquivo {file}...')
+        df.append(pd.read_excel(os.path.join(local_files, file)))
+
+df_master = pd.concat(df, axis=0)
+df_master.to_excel('base_dados/union_bases.xlsx', index=False)
